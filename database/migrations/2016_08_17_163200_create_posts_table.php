4c059d4428ee3b79f0f5463131611ab7b2c7e983
jay nichols <jay_nichols_3@hotmail.com>
2016-08-17 11:53:37 -0500

    databases, tables, with laravel

---
+++ database/migrations/2016_08_17_163200_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('posts', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('title', 200);
+            $table->string('url');
+            $table->string('content');
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->timeStamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
